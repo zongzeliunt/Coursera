044b7aa2e6622005c7acde11eec43134ff9efd26
Zongze Li <zongzeli2@my.unt.edu>
2020-11-05 23:52:34 -0600

    update TF2 nn cases

--- TF2/nn/4_cnn.py
+++ TF2/nn/4_cnn.py
@@ -2,15 +2,44 @@
 
 import matplotlib.pyplot as plt
 import tensorflow
-import tensorflow.keras as K
+import keras  
 import numpy as np
-from tensorflow.keras import layers
+from keras import layers
 
-(x_train, y_train), (x_test, y_test) = K.datasets.mnist.load_data()
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 x_train = x_train.reshape((-1,28,28,1))
 x_test = x_test.reshape((-1,28,28,1))
 print(x_train.shape, ' ', y_train.shape)
 print(x_test.shape, ' ', y_test.shape)
+
+x_shape  = x_train.shape
+deep_model = keras.Sequential(
+[
+    layers.Conv2D(input_shape=((x_shape[1], x_shape[2], x_shape[3])),
+                 filters=32, kernel_size=(3,3), strides=(1,1), padding='same', activation='relu'),
+    layers.Conv2D(filters=32, kernel_size=(3,3), strides=(1,1), padding='same', activation='relu'),
+    layers.MaxPool2D(pool_size=(2,2)),
+    layers.Conv2D(filters=32, kernel_size=(3,3), strides=(1,1), padding='same', activation='relu'),
+    layers.Conv2D(filters=32, kernel_size=(3,3), strides=(1,1), padding='same', activation='relu'),
+    layers.MaxPool2D(pool_size=(2,2)),
+    layers.Flatten(),
+    layers.Dense(32, activation='relu'),
+    layers.Dense(10, activation='softmax')
+
+])
+
+deep_model.compile(optimizer=keras.optimizers.Adam(),
+             loss=keras.losses.SparseCategoricalCrossentropy(),
+            metrics=['accuracy'])
+
+deep_model.summary()
+
+history = deep_model.fit(x_train, y_train, batch_size=64, epochs=5, validation_split=0.1)
+
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.legend(['training', 'valivation'], loc='upper left')
+plt.show()
 
 
 
